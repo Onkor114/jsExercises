beb9a4ca1ad2f95181b8554a0b56afb3ca47c433
Onkor114 <abdinourahmed114@gmail.com>
2024-12-09 20:20:20 +0300

    updated exercise js

--- exercise #36/main.js
+++ exercise #36/main.js
@@ -19,31 +19,3 @@ clearHistoryColor.addEventListener('click', function() {
     colorHistory.innerHTML = "";
 });
 
-// const colorPicker = document.querySelector("#colorpicker");
-// const colorPreview = document.querySelector("#colorpreview");
-// const colorHistory = document.querySelector("#colorhistory");
-// const clearHistoryButton = document.querySelector("#clearhistoryButton");
-
-// // Check if the elements exist before attaching event listeners
-// if (colorPicker && colorPreview && colorHistory && clearHistoryButton) {
-//     // Event listener for color changes
-//     colorPicker.addEventListener("input", function() {
-//         const selectedColor = colorPicker.value;
-//         colorPreview.style.backgroundColor = selectedColor;
-//         addColorToHistory(selectedColor);
-//     });
-
-//     // Function to add the selected color to the history
-//     function addColorToHistory(color) {
-//         const li = document.createElement("li");
-//         li.textContent = color;
-//         li.style.color = color;
-//         colorHistory.appendChild(li);
-//     }
-
-//     // Event listener for clearing history
-//     clearHistoryButton.addEventListener("click", function() {
-//         colorHistory.innerHTML = ""; // Clear the color history list
-
-//     });
-// };
